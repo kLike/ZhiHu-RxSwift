d30932c4d4324e2dc1e480d15bae889fe59cd402
“klike” <2283529034@qq.com>
2017-02-14 11:38:47 +0800

    主要功能都已完成

--- ZhiHu+RxSwift/DetailViewController.swift
+++ ZhiHu+RxSwift/DetailViewController.swift
@@ -16,7 +16,26 @@ import RxDataSources
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var webview: UIWebView!
-    var img = UIImageView()
+    var img = UIImageView().then {
+        $0.frame = CGRect.init(x: 0, y: 0, width: screenW, height: 200)
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        let maskImg = UIImageView.init(frame: CGRect.init(x: 0, y: 100, width: screenW, height: 100))
+        maskImg.image = UIImage.init(named: "Home_Image_Mask")
+        $0.addSubview(maskImg)
+    }
+    var titleLab = UILabel().then {
+        $0.frame = CGRect.init(x: 15, y: 150, width: screenW - 30, height: 26)
+        $0.font = UIFont.boldSystemFont(ofSize: 21)
+        $0.numberOfLines = 2
+        $0.textColor = UIColor.white
+    }
+    var imgLab = UILabel().then {
+        $0.frame = CGRect.init(x: 15, y: 180, width: screenW - 30, height: 16)
+        $0.font = UIFont.systemFont(ofSize: 10)
+        $0.textAlignment = .right
+        $0.textColor = UIColor.white
+    }
     let provider = RxMoyaProvider<ApiManager>()
     let dispose = DisposeBag()
     var id = Int() {
@@ -29,10 +48,9 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: UILabel())
-        img.frame = CGRect.init(x: 0, y: 0, width: screenW, height: 200)
-        img.contentMode = .scaleAspectFill
-        img.clipsToBounds = true
         webview.scrollView.addSubview(img)
+        img.addSubview(titleLab)
+        img.addSubview(imgLab)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,7 +71,18 @@ extension DetailViewController {
             .request(.getNewsDesc(id))
             .mapModel(NewsDetailModel.self)
             .subscribe(onNext: { (model) in
-                self.img.kf.setImage(with: URL.init(string: model.image ?? ""))
+                if let image = model.image {
+                    self.img.kf.setImage(with: URL.init(string: image))
+                    self.titleLab.text = model.title
+                } else {
+                    self.img.isHidden = true
+                }
+                if let image_source = model.image_source {
+                    self.imgLab.text = "图片: " + image_source
+                }
+                if (model.title?.characters.count)! > 16 {
+                   self.titleLab.frame = CGRect.init(x: 15, y: 120, width: screenW - 30, height: 55)
+                }
                 OperationQueue.main.addOperation {
                     self.webview.loadHTMLString(self.concatHTML(css: model.css!, body: model.body!), baseURL: nil)
                 }
