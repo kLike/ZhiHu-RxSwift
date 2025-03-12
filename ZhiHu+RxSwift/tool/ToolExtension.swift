073ff8c068269b7a3f16ee8c4dd28c89711ecc93
kLike <klikecode@163.com>
2025-03-12 09:53:53 +0800

    Update ToolExtension.swift

--- ZhiHu+RxSwift/tool/ToolExtension.swift
+++ ZhiHu+RxSwift/tool/ToolExtension.swift
@@ -37,8 +37,8 @@ extension Int {
             return "星期一"
         case 3:
             return "星期二"
-        case 4:
-            return "星期三"
+        // case 4:
+        //     return "星期三"
         case 5:
             return "星期四"
         case 6:
